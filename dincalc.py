3f2eb2829f200a54df666299a23b6d09e93f67c8
Richard Williams <richyw@uvic.ca>
2016-10-20 21:44:50 -0600

    Added age and skier type corrections

--- dincalc.py
+++ dincalc.py
@@ -9,22 +9,45 @@ boot_length = input('Boot Sole Length: ')
 
 # Compute Uncorrected Skier Code
 # Assigns a number from 0-14 corresponding to skier code from a-o
-skier_type = 0; # start with type zero
+#skier_code = 0; # start with type zero
 
-weight_code = [0,30,39,48,57,67,79,92,108,126,148,175,210]
-height_code = [0,149,158,167,179,195]
-letter_code = list(string.uppercase)
+weight_range = [0,30,39,48,57,67,79,92,108,126,148,175,210]
+height_range = [0,149,158,167,179,195]
+letter_code = list(string.uppercase) # To convert Skier code to letter
 
-# loop through weight code array and assign skier type based on weight
-for index in range(len(weight_code)):
-    if weight >= weight_code[index]:
-        skier_type = index
+# loop through weight code array weight code
+for index in range(len(weight_range)):
+    if weight >= weight_range[index]:
+        weight_code = index
 
-# loop through code and assign skier type based on height
-for index in range(len(height_code)):
-    constant = len(weight_code) - len(height_code) # to account for array length difference
-    if height >= height_code[index] and index + constant < skier_type:
-        skier_type = index + constant
+# loop through code and assign height code
+constant = len(weight_range) - len(height_range) # to account for array length difference
+for index in range(len(height_range)):
+    if height >= height_range[index]:
+        height_code = index + constant
+
+#assign skier code to be height code or the weight code - whichever is lower
+if height_code < weight_code:
+    skier_code = height_code
+else:
+    skier_code = weight_code
+
+#print skier code
+print(letter_code[skier_code])
+
+#make corrections for age
+if age <= 9 or age > 50:
+    skier_code -= 1
+
+#make corrections for skier type
+if skier_type == "-1":
+    skier_code -=1
+
+elif skier_type == "3+":
+    skier_code += 3
+
+else:
+    skier_code += skier_type - 1
 
 #print skier type
-print(letter_code[skier_type])
+print(letter_code[skier_code])
